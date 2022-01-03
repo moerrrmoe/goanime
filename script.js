0ea1cf0ab43a936bc2535d003617d53cb442d429
moerrrmoe <92532476+moerrrmoe@users.noreply.github.com>
2022-01-03 22:01:35 +0630

    Create script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+<script src="http://www.geoplugin.net/javascript.gp" type="text/javascript"></script>
+  
+<script> 
+	window.onload=function onload() {
+					if(geoplugin_countryName()=="Myanmar") {
+									document.getElementById("a").href="";
+					}
+	}
+</script>
