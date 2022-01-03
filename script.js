609a2490ca5e1d4a2c7500300c2e85ecdd138b98
moerrrmoe <92532476+moerrrmoe@users.noreply.github.com>
2022-01-03 22:12:30 +0630

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,6 @@
-<script src="http://www.geoplugin.net/javascript.gp" type="text/javascript"></script>
-  
-<script> 
+
 	window.onload=function onload() {
 					if(geoplugin_countryName()=="Myanmar") {
 						window.location.href="https://bing.com"
 					}
 	}
-</script>
